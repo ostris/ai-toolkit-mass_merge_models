9da326b940967088bda6fbf42fb76d98a97ce88c
Jaret Burkett <jaretburkett@gmail.com>
2023-08-13 08:49:09 -0600

    Mass merge working well as is. Want to rework naming conventions so we can reuse all caches for other merges if wanted

--- MassMergeModelsCSI.py
+++ MassMergeModelsCSI.py
@@ -5,20 +5,21 @@ import shutil
 import torch
 import gc
 from collections import OrderedDict
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Union, List
 
+from torch.nn import functional as F
+
+import yaml
 from safetensors.torch import save_model, save_file, load_file
 
 from jobs.process import BaseExtensionProcess
+from toolkit.basic import value_map
 from toolkit.config_modules import ModelConfig
 from toolkit.paths import get_path
 from toolkit.stable_diffusion_model import StableDiffusion
 from toolkit.train_tools import get_torch_dtype
 from tqdm import tqdm
-from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-from scipy.cluster.hierarchy import linkage, fcluster
-import pickle
 
 # Type check imports. Prevents circular imports
 if TYPE_CHECKING:
@@ -40,15 +41,10 @@ def flush():
     gc.collect()
 
 
-class NumpyEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return json.JSONEncoder.default(self, obj)
-
-
 # this is our main class process
 class MassMergeModelsCSI(BaseExtensionProcess):
+    loss_fn: Union[F.mse_loss, F.l1_loss, F.cosine_similarity]
+    models_to_merge: List[ModelInputConfig]
     def __init__(
             self,
             process_id: int,
@@ -60,12 +56,26 @@ class MassMergeModelsCSI(BaseExtensionProcess):
         self.working_dir = get_path(self.get_conf('working_dir', required=True))
         self.save_dtype = self.get_conf('save_dtype', default='float16')
         self.device = self.get_conf('device', default='cpu')
-        self.passes = self.get_conf('passes', default=1, as_type=int)
+        self.report_format = self.get_conf('report_format', default='json')
+        self.keep_cache = self.get_conf('keep_cache', default=False, as_type=bool)
+        if self.report_format == 'null':
+            self.report_format = None
+
         self.cache_dtype = self.get_conf('cache_dtype', default='float32', as_type=get_torch_dtype)
         self.base_model = ModelInputConfig(**self.get_conf('base_model', required=True))
         # merge_step = self.get_conf('merge_step', default=1, as_type=int)
 
         self.weight_json = os.path.join(self.working_dir, 'weight.json')
+        self.differential_loss = self.get_conf('differential_loss', 'mse')
+
+        if self.differential_loss == 'mse':
+            self.loss_fn = F.mse_loss
+        elif self.differential_loss == 'l1' or self.differential_loss == 'mae':
+            self.loss_fn = F.l1_loss
+        elif self.differential_loss == 'cosine':
+            self.loss_fn = F.cosine_similarity
+        else:
+            raise ValueError(f"Invalid differential loss {self.differential_loss}")
         # make working dir
         os.makedirs(self.working_dir, exist_ok=True)
 
@@ -140,8 +150,6 @@ class MassMergeModelsCSI(BaseExtensionProcess):
                     base_te = [base_model.text_encoder]
                     instance_te = [sd_model.text_encoder]
 
-
-
                 te_num = 0
                 for text_encoder, base_text_encoder in zip(instance_te, base_te):
                     for key, value in text_encoder.state_dict().items():
@@ -203,52 +211,65 @@ class MassMergeModelsCSI(BaseExtensionProcess):
 
         num_models = len(self.models_to_merge)
 
-        tensor_sim_path = os.path.join(self.working_dir, self.name, 'tensor_sim.pkl')
+        ### COMPUTE COSINE SIMILARITY ###
+
+        # tensor_sim_path = os.path.join(self.working_dir, self.name, 'tensor_sim.pkl')
+        tensor_sim_path = os.path.join(self.working_dir, self.name, f'tensor_sim_{self.differential_loss}.safetensors')
         if os.path.exists(tensor_sim_path):
-            with open(tensor_sim_path, 'rb') as handle:
-                tensor_similarities = pickle.load(handle)
+            tensor_similarities = load_file(tensor_sim_path, self.device)
+            print(f"Loaded tensor similarities from {tensor_sim_path}")
+            print(" - Checking for new items to compute")
         else:
 
             tensor_similarities = {}
 
-            pbar = tqdm(total=len(tensor_keys), desc="Computing cosine similarity")
+            num_similarities_added = 0
 
-            ### COMPUTE COSINE SIMILARITY ###
+            pbar = tqdm(total=len(tensor_keys), desc="Computing similarity")
 
             # calculate the similarity matrix for each tensor
             for key in tensor_keys:
-                similarity_matrix = np.zeros((num_models, num_models))
-                # tensors = [torch.load(f"{key}_{idx}.pt") for idx in range(num_models)]
-                tensors_paths = [os.path.join(cache_dir, f"{key.replace('[idx]', str(idx))}.safetensors") for idx in
-                                 range(num_models)]
+                similarity_matrix = torch.zeros((num_models, num_models), device=self.device, dtype=self.cache_dtype)
+
+                tensors_paths = [
+                    os.path.join(
+                        cache_dir, f"{key.replace('[idx]', str(idx))}.safetensors"
+                    ) for idx in range(num_models)
+                ]
 
                 for i in range(num_models):
                     tensors_i = load_file(tensors_paths[i], self.device)['x']
-                    tensors_i = tensors_i.cpu().numpy().reshape(1, -1)
+                    if self.differential_loss == 'cosine':
+                        tensors_i = tensors_i.view(1, -1)
                     for j in range(i + 1, num_models):
                         tensors_j = load_file(tensors_paths[j], self.device)['x']
-                        tensors_j = tensors_j.cpu().numpy().reshape(1, -1)
-                        sim = cosine_similarity(tensors_i, tensors_j)
-                        similarity_matrix[i, j] = sim[0][0]
-                        similarity_matrix[j, i] = sim[0][0]
+                        if self.differential_loss == 'cosine':
+                            tensors_j = tensors_j.view(1, -1)
+                            sim = self.loss_fn(tensors_i, tensors_j)
+                            # do abs incase it is cosine similarity
+                            sim = torch.abs(sim)
+                        else:
+                            sim = self.loss_fn(tensors_i, tensors_j)
+                        similarity_matrix[i, j] = sim
+                        similarity_matrix[j, i] = sim
                         del tensors_j
                     del tensors_i
                     flush()
 
                 tensor_similarities[key] = similarity_matrix
+                num_similarities_added += 1
                 pbar.update(1)
 
             pbar.close()
 
-            # save the similarity matrix
-            with open(tensor_sim_path, 'wb') as handle:
-                pickle.dump(tensor_similarities, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
+            if num_similarities_added > 0:
+                print(f"Added {num_similarities_added} new similarities")
+                print(f"Saving tensor similarities to {tensor_sim_path}")
+                save_file(tensor_similarities, tensor_sim_path)
 
         ### COMPUTE WEIGHTS FOR EACH TENSOR ###
 
-        print("Computing weights for each tensor using clustering")
+        print("Reducing feature similarity matrices")
 
         # compute the weights for each tensor based on the similarity matrix
         tensor_weights = {}
@@ -256,21 +277,35 @@ class MassMergeModelsCSI(BaseExtensionProcess):
         # we can use a few different methods to compute the weights
         # I will just continue with clustering for now
 
+        # will keep the weight at minimum 10% of the model activity scaled to number of models
+        # min_model_activity = 0.1
+        # min_weight_value = min_model_activity / len(self.models_to_merge)
+
         for key, similarity_matrix in tensor_similarities.items():
-            # reduce the sum along the first axis to get the sum of similarities for each tensor
-            # this is the number of clusters we want
-            weights = np.sum(similarity_matrix, axis=0)
-            # normalize the weights, reduce sum to 1
-            weights = weights / np.sum(weights)
+            # move minimum to 0 to compensate for negative similarities
+            # similarity_matrix -= torch.min(similarity_matrix)
+            weights = torch.sum(similarity_matrix, dim=0)
+            # todo, try different scaling methods here
+
+            weights = value_map(weights, torch.min(weights), torch.max(weights), 0, 1)
+
+            # scale weights so the sum is 1
+            weights = weights / torch.sum(weights)
+
+
+            # scale weights from min_weight_value to 1
+            # weights = value_map(weights, 0, torch.max(weights), 0, 1)
+            # weights = value_map(weights, torch.min(weights), torch.max(weights), min_weight_value, 1)
+
             tensor_weights[key] = weights
 
         ### WEIGHING AND MERGING ###
 
-        print("Weighing and merging tensors")
+        print("Building merged feature ensemble")
 
         ensemble_state_dict = {}
 
-        pbar = tqdm(total=len(tensor_keys), desc="Merging ensemble tensors")
+        pbar = tqdm(total=len(tensor_keys), desc="Building ensemble")
 
         for key in tensor_keys:
             weighted_sum = torch.zeros_like(base_state_dict_all[key])
@@ -292,6 +327,8 @@ class MassMergeModelsCSI(BaseExtensionProcess):
 
         pbar.close()
 
+        ### MERGE INTO BASE MODEL ###
+
         print("Merging ensemble model back into base model")
 
         # merge the ensemble state dict into the base model
@@ -310,16 +347,53 @@ class MassMergeModelsCSI(BaseExtensionProcess):
 
         base_model.save(self.save_path, meta=self.meta, save_dtype=self.save_dtype)
 
-        # save tensor_weights as json to disk next to the model with the same name different extension
-        with open(self.save_path.replace(".safetensors", ".json"), 'w') as f:
-            json.dump(tensor_weights, f, cls=NumpyEncoder, indent=4)
+        if self.report_format is not None:
+            # create tensor merge report
+            merge_report = OrderedDict()
+            merge_report['totals'] = OrderedDict()
+            num_total_keys = len(list(tensor_weights.keys()))
+            for key, weights in tensor_weights.items():
+                weights = weights.detach().cpu().numpy()
+                # convert to list of floats
+                weights = [float(w) for w in weights]
+                clean_key = key.replace("_m[idx]", "")
+                merge_report.setdefault(clean_key, OrderedDict())
+                for weight, model_config in zip(weights, self.models_to_merge):
+                    model_path = model_config.name_or_path
+                    # get filename no ext. Can be any extension
+                    filename_no_ext = os.path.splitext(os.path.basename(model_path))[0]
+                    merge_report[clean_key][filename_no_ext] = weight
+                    if filename_no_ext not in merge_report['totals']:
+                        merge_report['totals'][filename_no_ext] = 0.0
+                    merge_report['totals'][filename_no_ext] += (weight / num_total_keys)
 
-        print(f"Saved weights {self.save_path.replace('.safetensors', '.json')}")
+            for key, weight_dict in merge_report.items():
+                # sort the weight dicts by value
+                merge_report[key] = OrderedDict(sorted(weight_dict.items(), key=lambda x: x[1], reverse=True))
 
-        print(f"Removing tmp directory {cache_dir}")
-        # shutil.rmtree(os.path.join(self.working_dir, self.name))
+
+            save_path_no_ext = os.path.splitext(self.save_path)[0]
+
+            saved_to = None
+            if self.report_format == "yaml":
+                with open(f"{save_path_no_ext}_report.yaml", 'w') as f:
+                    yaml.dump(merge_report, f)
+                    saved_to = f"{save_path_no_ext}_report.yaml"
+            elif self.report_format == "json":
+                with open(f"{save_path_no_ext}_report.json", 'w') as f:
+                    json.dump(merge_report, f, indent=4)
+                    saved_to = f"{save_path_no_ext}_report.json"
+
+            print(f"Saved merge report to {saved_to}")
+
+        if not self.keep_cache:
+            print(f"Removing tmp directory {cache_dir}")
+            shutil.rmtree(os.path.join(self.working_dir, self.name))
+        else:
+            print(f"Keeping tmp directory {cache_dir}")
+            print(" - Be sure to manually clean it up, as it takes up a lot of space")
         del base_model
         del ensemble_state_dict
         flush()
 
-        print("We did it! Go try it out!")
+        print("Finished!")
